ba2fb8b7016c092f259f1e2b3fcd3b6ccc2ee725
0ivaan0 <ivanperezminguez@gmail.com>
2024-02-20 14:10:36 +0100

    ESTEBAN PAGAME

--- src/polonorte/Reno.java
+++ src/polonorte/Reno.java
@@ -24,7 +24,7 @@ public class Reno {
 
 
 	public String volar(String direccion){
-        System.out.println("Reno.volar");
+        System.out.println("Reno.volar SISIISISISISISISISSISIISISISISIISISISISISISISISISISISISISISISISISISISISIISISISISSISIISISISISISISISISISISISISISISISISISISIISISIS");
 
         return "mmmmmiiiiii chus chus peeeeerrrr";
     }
