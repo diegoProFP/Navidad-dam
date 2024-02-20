481ea698001f36b85f3b44209babf1142e21fcda
diegoProFP <97691170+diegoProFP@users.noreply.github.com>
2024-02-20 14:08:38 +0100

    Update Reno.java

--- src/polonorte/Reno.java
+++ src/polonorte/Reno.java
@@ -24,8 +24,8 @@ public class Reno {
 
 
 	public String volar(String direccion){
-        System.out.println("Reno.volar");
-
+        System.out.println("Reno Renardo");
+System.out.println("vuela vuela");
         return "mmmmmiiiiii chus chus peeeeerrrr";
     }
 
