eefb455aa9a523fd88871ee8a29792c28bea8b08
AlvaroSeguraDelBarrio <alvaroseguradelbarrio@gmal.com>
2024-02-20 13:57:33 +0100

    alvaro

--- src/polonorte/elfos/Alvaro.java
+++ src/polonorte/elfos/Alvaro.java
@@ -5,4 +5,9 @@ public class Alvaro extends ElfoGenerico {
 	public Alvaro() {
 		super("AlvaroExtraño", SeccionLaboralElfo.DISFRACES);
 	}
+	int contador=44;
+	while (contador!0){
+		System.out.println("dime algo")
+		contador--
+	}
 }
