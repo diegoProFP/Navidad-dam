fcfab1ee407c85e3ffc0071899d536f42ec6e7a2
amandaSMZ <amandamigut@gmail.com>
2024-02-20 13:48:07 +0100

    JaAlberto añadido

--- src/polonorte/elfos/JaviClass.java
+++ src/polonorte/elfos/JaviClass.java
@@ -2,5 +2,5 @@ package polonorte.elfos;
 
 public class JaviClass extends ElfoGenerico{
     public JaviClass{
-        super(nombre: "Alberto", SeccionLaboralElfo.DISFRACES);}
+        super(nombre "Alberto", SeccionLaboralElfo.DISFRACES);}
 }
