a91499db14c0b562bb2cc54776b4b1f343fc69f0
MaroBrihuega <marcobrihuega@gmail.com>
2024-02-20 13:45:25 +0100

    Liberto es un psicopata

---
+++ src/polonorte/elfos/Marco.java
@@ -0,0 +1,11 @@
+package polonorte.elfos;
+
+public class Marco extends ElfoGenerico{
+
+    double altura;
+
+    protected Marco(String nombre, Enum seccionLaboral,double altura) {
+        super(nombre, seccionLaboral);
+        this.altura=altura;
+    }
+}
