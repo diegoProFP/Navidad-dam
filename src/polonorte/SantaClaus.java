d266f70e0c4f146e56f914578cd6ca2d0ccca1bc
diegoProFP <diego.di1@educa.madrid.org>
2024-03-04 09:55:25 +0100

    Revert "Comentario 1"
    This reverts commit 43f58cbf

--- src/polonorte/SantaClaus.java
+++ src/polonorte/SantaClaus.java
@@ -6,8 +6,6 @@ import java.time.Month;
 //Esta clase describe a un santa claus y la creó Baltasar. Se considera que es la versión 1.2 del programa
 public class SantaClaus {
 
-	//Comentario 1
-	//Comentario 2
 	// Los kilos de peso de SantaClaus
 	private int kilosPeso;
 
